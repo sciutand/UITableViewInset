58678d5934baac2e0372cce6000f590ec7d93b4a
Andrea Sciutto <sciutand@gmail.com>
2017-11-20 09:51:09 +1300

    Add labels

--- TableViewInset/ViewController.swift
+++ TableViewInset/ViewController.swift
@@ -10,27 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var behaviourButton: UIBarButtonItem!
 	@IBOutlet weak var tableview: UITableView!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		if  #available(iOS 11, *) {
-			tableview.contentInsetAdjustmentBehavior = .never
-		}
-		
+		navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+		navigationController?.navigationBar.shadowImage = UIImage()
+		navigationController?.navigationBar.isTranslucent = true
 		tableview.contentInset = UIEdgeInsets(top: 200, left: 0, bottom: 0, right: 0)
+		if  #available(iOS 11, *) {
+			setBehaviour(behaviour: .never, title: "Never")
+		}
+	}
+	
+	@available(iOS 11, *)
+	func setBehaviour(behaviour: UIScrollViewContentInsetAdjustmentBehavior, title: String) {
+		behaviourButton.title = title
+		tableview.contentInsetAdjustmentBehavior = behaviour
+	}
+	
+	@available(iOS 11, *)
+	func getBehaviours() -> [String: UIScrollViewContentInsetAdjustmentBehavior]{
+		return ["Never": .never, "Automatic": .automatic, "Always": .always, "Axes": .scrollableAxes]
 	}
 
 	@IBAction func toggle(_ sender: Any) {
 		guard #available(iOS 11, *) else {
 			return
 		}
-		let actions:[String: UIScrollViewContentInsetAdjustmentBehavior] =
-			["Never": .never, "Automatic": .automatic, "Always": .always, "Axes": .scrollableAxes]
+		
+			
 		let alert = UIAlertController(title: "Change behaviour", message: "", preferredStyle: .actionSheet)
 		
-		actions.forEach { title, behaviour in
+		getBehaviours().forEach { title, behaviour in
 			alert.addAction(UIAlertAction(title: title, style: .default) { action in 
-			self.tableview.contentInsetAdjustmentBehavior = behaviour
+				self.setBehaviour(behaviour: behaviour, title: title)
 			})
 		}
 		present(alert, animated: true, completion: nil)
